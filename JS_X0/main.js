aa1dad182fa3818edd35864d4e64bdf5bf39de81
ssamoschenkov <ssamoschenkov@gmail.com>
2020-10-20 22:29:35 +0300

    1 версия

---
+++ JS_X0/main.js
@@ -0,0 +1,58 @@
+const area = document.getElementById('area');
+let move = 0;
+const contentWrapper = document.getElementById('content');
+const modalResult = document.getElementById('modal-result-wrapper');
+const overlay = document.getElementById('overlay');
+const btnClose = document.getElementById('btn-close');
+
+// Комментарий
+// const area = document.getElementById('area');
+area.addEventListener('click', e => {
+	if (e.target.classname = 'box') {
+		// console.log(e.target)
+		move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0';
+		move++;
+		check();
+	}
+})
+
+const check = () => {
+	const boxes = document.getElementsByClassName('box');
+	// console.log(boxes);
+	const arr = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (i = 0; i < arr.length; i++) {
+		if (
+			boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X'
+		) {
+			result = 'крестики';
+			prepareResult(result);
+		} else if (
+			boxes[arr[i][0]].innerHTML == '0' && boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0'
+		) {
+			result = 'нолики';
+			prepareResult(result);
+		}
+	}
+}
+
+const prepareResult = winner => {
+	// console.log(winner);
+	contentWrapper.innerHTML = `Победили ${winner} !`;
+	modalResult.style.display = 'block';
+}
+
+const closeModal = () => {
+	modalResult.style.display = 'none';
+	location.reload();
+}
+overlay.addEventListener('click', closeModal);
+btnClose.addEventListener('click', closeModal);
